96bb7916dc70fcd022ec73b4a5f34ef709446de3
emanuele5180 <macina5180>
2020-09-02 15:50:48 +0200

    inizio, impostazione dell'array

---
+++ index.php
@@ -0,0 +1,28 @@
+<!-- Snack 1
+Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema. -->
+
+
+
+<?php
+
+  $giornataUno = [
+    "partitaUno" => [
+      "squadra di casa" => "Olimpia Milano",
+      "squadra ospite" => "Cantù",
+      "punteggio squadra di casa" => 55,
+      "punteggio squadra ospite" => 60
+    ]
+
+    echo$partitaUno["squadra di casa"];
+    // $partitaDue = [],
+    // $partitaTre = [],
+    // $partitaQuattro = [],
+    // $partitaCinque = []
+    //
+  ];
+  var_dump($giornataUno);
+
+
+
+
+?>
